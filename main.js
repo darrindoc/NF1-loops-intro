4524a0889a24be5e0528de3bdcb2623e4c285f66
Darrin Daugherty <darrin@darrin.pro>
2023-08-30 09:22:36 -0400

    Loop exercises because I am a rockstar.

---
+++ main.js
@@ -0,0 +1,17 @@
+//Printing C1 Student names
+
+/*
+
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+
+for(var i = 0; i < cohortOneStudents.length; i++){
+    console.log(cohortOneStudents[i]);
+}
+
+*/
+
+var darrinInterests = ["Programming", "Video Games", "Walking my dog", "Documentaries", "Home Imporvement"]
+
+for (var i = 0; i <darrinInterests.length; i++){
+    console.log(`One of my interests is ${darrinInterests[i]}.`)
+}
